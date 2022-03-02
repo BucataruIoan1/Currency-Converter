3abf1ac99c9d4ab1d27828a656d203297aee32fb
Bucataru Ioan <bucataruioan1@gmail.com>
2022-03-02 12:19:34 +0200

    Reverse the currency values

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let inputElement = document.querySelector('#amountValue');
 let resultFirstElement = document.querySelector('.resultExchange p');
+let reverseButtonElement = $(".reverseButton")[0];
 
 // Define the value of currency
 
@@ -71,6 +72,8 @@ let selectTagSecondElement = document.querySelector("#currencyTypeSecond").addEv
     }
 });
 
+// Check the input and apeal the 'calculateCurrency' function
+
 function checkNumberInput() {
 
     let checkNumber = Number(inputElement.value);
@@ -92,53 +95,6 @@ function checkNumberInput() {
         calculateCurrency(".forint", forintValue);
         calculateCurrency(".zloty", zlotyValue);
 
-        function calculateCurrency(currency, currencyValue) {
-
-        if ($(currency)[0].selected === true && $(".euro")[1].selected === true) {
-        resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / euroValue) * currencyValue) +  ' ' + $(".euro")[0].innerHTML;
-        }
-        else if ($(currency)[0].selected === true && $(".ron")[1].selected === true) {
-            resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / ronValue) * currencyValue) +  ' ' + $(".ron")[0].innerHTML;
-        }
-        else if ($(currency)[0].selected === true && $(".dollar")[1].selected === true) {
-            resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / dollarValue) * currencyValue) +  ' ' + $(".dollar")[0].innerHTML;
-        }
-        else if ($(currency)[0].selected === true && $(".pound")[1].selected === true) {
-            resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / poundValue) * currencyValue) +  ' ' + $(".pound")[0].innerHTML;
-        }
-        else if ($(currency)[0].selected === true && $(".franc")[1].selected === true) {
-            resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / francValue) * currencyValue) +  ' ' + $(".franc")[0].innerHTML;
-        }
-        else if ($(currency)[0].selected === true && $(".canadian")[1].selected === true) {
-            resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / canadianValue) * currencyValue) +  ' ' + $(".canadian")[0].innerHTML;
-        }
-        else if ($(currency)[0].selected === true && $(".yen")[1].selected === true) {
-            resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / yenValue) * currencyValue) +  ' ' + $(".yen")[0].innerHTML;
-        }
-        else if ($(currency)[0].selected === true && $(".yuan")[1].selected === true) {
-            resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / yuanValue) * currencyValue) +  ' ' + $(".yuan")[0].innerHTML;
-        }
-        else if ($(currency)[0].selected === true && $(".rupee")[1].selected === true) {
-            resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / rupeeValue) * currencyValue) +  ' ' + $(".rupee")[0].innerHTML;
-        }
-        else if ($(currency)[0].selected === true && $(".rouble")[1].selected === true) {
-            resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / roubleValue) * currencyValue) +  ' ' + $(".rouble")[0].innerHTML;
-        }
-        else if ($(currency)[0].selected === true && $(".lev")[1].selected === true) {
-            resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / levValue) * currencyValue) +  ' ' + $(".lev")[0].innerHTML;
-        }
-        else if ($(currency)[0].selected === true && $(".koruna")[1].selected === true) {
-            resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / korunaValue) * currencyValue) +  ' ' + $(".koruna")[0].innerHTML;
-        }
-        else if ($(currency)[0].selected === true && $(".forint")[1].selected === true) {
-            resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / forintValue) * currencyValue) +  ' ' + $(".forint")[0].innerHTML;
-        }
-        else if ($(currency)[0].selected === true && $(".zloty")[1].selected === true) {
-            resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / zlotyValue) * currencyValue) +  ' ' + $(".zloty")[0].innerHTML;
-        }
-
-    }
-
     } else {
         inputElement.value = '';
         resultFirstElement.innerHTML = '';
@@ -147,6 +103,8 @@ function checkNumberInput() {
     inputElement.value = '';
     
 }
+
+// Select the FLAG
 
 function showFlag(optionElement, flag, index0, index1, index2) {
 
@@ -196,3 +154,97 @@ function showFlag(optionElement, flag, index0, index1, index2) {
     }
 
 }
+
+// Calculate the Currency Values and Reverse then
+
+function calculateCurrency(currency, currencyValue) {
+
+let inputValue = inputElement.value;
+
+    if ($(currency)[0].selected === true && $(".euro")[1].selected === true) {
+    resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / euroValue) * currencyValue).toFixed(3) +  ' ' + $(".euro")[0].innerHTML;
+    reverseButtonElement.addEventListener("click", function() {
+        resultFirstElement.innerHTML = inputValue + ' ' + $('.euro')[0].innerHTML + ' = ' + ((inputValue / currencyValue) * euroValue).toFixed(3) + ' ' + $(currency)[0].innerHTML;
+    });    
+    }
+    else if ($(currency)[0].selected === true && $(".ron")[1].selected === true) {
+        resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / ronValue) * currencyValue).toFixed(3) +  ' ' + $(".ron")[0].innerHTML; 
+        reverseButtonElement.addEventListener("click", function() {
+            resultFirstElement.innerHTML = inputValue + ' ' + $('.ron')[0].innerHTML + ' = ' + ((inputValue / currencyValue) * ronValue).toFixed(3) + ' ' + $(currency)[0].innerHTML;
+        });  
+    }
+    else if ($(currency)[0].selected === true && $(".dollar")[1].selected === true) {
+        resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / dollarValue) * currencyValue).toFixed(3) +  ' ' + $(".dollar")[0].innerHTML; 
+        reverseButtonElement.addEventListener("click", function() {
+            resultFirstElement.innerHTML = inputValue + ' ' + $('.dollar')[0].innerHTML + ' = ' + ((inputValue / currencyValue) * dollarValue).toFixed(3) + ' ' + $(currency)[0].innerHTML;
+        });  
+    }
+    else if ($(currency)[0].selected === true && $(".pound")[1].selected === true) {
+        resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / poundValue) * currencyValue).toFixed(3) +  ' ' + $(".pound")[0].innerHTML;
+        reverseButtonElement.addEventListener("click", function() {
+            resultFirstElement.innerHTML = inputValue + ' ' + $('.pound')[0].innerHTML + ' = ' + ((inputValue / currencyValue) * poundValue).toFixed(3) + ' ' + $(currency)[0].innerHTML;
+        });  
+    }
+    else if ($(currency)[0].selected === true && $(".franc")[1].selected === true) {
+        resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / francValue) * currencyValue).toFixed(3) +  ' ' + $(".franc")[0].innerHTML;
+        reverseButtonElement.addEventListener("click", function() {
+            resultFirstElement.innerHTML = inputValue + ' ' + $('.franc')[0].innerHTML + ' = ' + ((inputValue / currencyValue) * francValue).toFixed(3) + ' ' + $(currency)[0].innerHTML;
+        });  
+    }
+    else if ($(currency)[0].selected === true && $(".canadian")[1].selected === true) {
+        resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / canadianValue) * currencyValue).toFixed(3) +  ' ' + $(".canadian")[0].innerHTML;
+        reverseButtonElement.addEventListener("click", function() {
+            resultFirstElement.innerHTML = inputValue + ' ' + $('.canadian')[0].innerHTML + ' = ' + ((inputValue / currencyValue) * canadianValue).toFixed(3) + ' ' + $(currency)[0].innerHTML;
+        });  
+    }
+    else if ($(currency)[0].selected === true && $(".yen")[1].selected === true) {
+        resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / yenValue) * currencyValue).toFixed(3) +  ' ' + $(".yen")[0].innerHTML;
+        reverseButtonElement.addEventListener("click", function() {
+            resultFirstElement.innerHTML = inputValue + ' ' + $('.yen')[0].innerHTML + ' = ' + ((inputValue / currencyValue) * yenValue).toFixed(3) + ' ' + $(currency)[0].innerHTML;
+        });  
+    }
+    else if ($(currency)[0].selected === true && $(".yuan")[1].selected === true) {
+        resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / yuanValue) * currencyValue).toFixed(3) +  ' ' + $(".yuan")[0].innerHTML;
+        reverseButtonElement.addEventListener("click", function() {
+            resultFirstElement.innerHTML = inputValue + ' ' + $('.yuan')[0].innerHTML + ' = ' + ((inputValue / currencyValue) * yuanValue).toFixed(3) + ' ' + $(currency)[0].innerHTML;
+        });  
+    }
+    else if ($(currency)[0].selected === true && $(".rupee")[1].selected === true) {
+        resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / rupeeValue) * currencyValue).toFixed(3) +  ' ' + $(".rupee")[0].innerHTML;
+        reverseButtonElement.addEventListener("click", function() {
+            resultFirstElement.innerHTML = inputValue + ' ' + $('.rupee')[0].innerHTML + ' = ' + ((inputValue / currencyValue) * rupeeValue).toFixed(3) + ' ' + $(currency)[0].innerHTML;
+        });  
+    }
+    else if ($(currency)[0].selected === true && $(".rouble")[1].selected === true) {
+        resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / roubleValue) * currencyValue).toFixed(3) +  ' ' + $(".rouble")[0].innerHTML;
+        reverseButtonElement.addEventListener("click", function() {
+            resultFirstElement.innerHTML = inputValue + ' ' + $('.rouble')[0].innerHTML + ' = ' + ((inputValue / currencyValue) * roubleValue).toFixed(3) + ' ' + $(currency)[0].innerHTML;
+        });  
+    }
+    else if ($(currency)[0].selected === true && $(".lev")[1].selected === true) {
+        resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / levValue) * currencyValue).toFixed(3) +  ' ' + $(".lev")[0].innerHTML;
+        reverseButtonElement.addEventListener("click", function() {
+            resultFirstElement.innerHTML = inputValue + ' ' + $('.lev')[0].innerHTML + ' = ' + ((inputValue / currencyValue) * levValue).toFixed(3) + ' ' + $(currency)[0].innerHTML;
+        });  
+    }
+    else if ($(currency)[0].selected === true && $(".koruna")[1].selected === true) {
+        resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / korunaValue) * currencyValue).toFixed(3) +  ' ' + $(".koruna")[0].innerHTML;
+        reverseButtonElement.addEventListener("click", function() {
+            resultFirstElement.innerHTML = inputValue + ' ' + $('.koruna')[0].innerHTML + ' = ' + ((inputValue / currencyValue) * korunaValue).toFixed(3) + ' ' + $(currency)[0].innerHTML;
+        });  
+    }
+    else if ($(currency)[0].selected === true && $(".forint")[1].selected === true) {
+        resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / forintValue) * currencyValue).toFixed(3) +  ' ' + $(".forint")[0].innerHTML;
+        reverseButtonElement.addEventListener("click", function() {
+            resultFirstElement.innerHTML = inputValue + ' ' + $('.forint')[0].innerHTML + ' = ' + ((inputValue / currencyValue) * forintValue).toFixed(3) + ' ' + $(currency)[0].innerHTML;
+        });  
+    }
+    else if ($(currency)[0].selected === true && $(".zloty")[1].selected === true) {
+        resultFirstElement.innerHTML = inputElement.value + ' ' + $(currency)[0].innerHTML + ' = ' + ((inputElement.value / zlotyValue) * currencyValue).toFixed(3) +  ' ' + $(".zloty")[0].innerHTML;
+        reverseButtonElement.addEventListener("click", function() {
+            resultFirstElement.innerHTML = inputValue + ' ' + $('.zloty')[0].innerHTML + ' = ' + ((inputValue / currencyValue) * zlotyValue).toFixed(3) + ' ' + $(currency)[0].innerHTML;
+        });  
+    }
+
+}
+
